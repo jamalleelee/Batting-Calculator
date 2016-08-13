2edc7aed1c4923240e68c1e892bf82aa54db2a40
Jamal <jamal.l.mcdowell@gmail.com>
2016-08-13 17:18:51 -0400

    completed batting calculator

--- BattingCalc.java
+++ BattingCalc.java
@@ -1,5 +1,6 @@
 package dayOne;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -7,17 +8,46 @@ import java.util.Scanner;
 public class BattingCalc {
 
 	public static void main(String[] args) {
-		int atBats;
+		double atBats=0;
 		double hits = 0;
 		ArrayList<Double> battingData = new ArrayList<Double>();
 		Scanner keyboard = new Scanner(System.in);
 		boolean isValid = true;
+		double totalHits=0;
+		boolean isAlsoValid=true;
+		boolean validHitNumber=true;
+		double sluggingPercentage=0;
+		double battingPercentage=0;
+		DecimalFormat df = new DecimalFormat("###.###");
 
-		System.out.println("How many times did you go to bat?");
-
-		atBats = keyboard.nextInt();
 		do {
-			for (int i = 0; i < atBats; i++) {
+			System.out.println("How many times did you go to bat?");
+			try {
+				atBats = keyboard.nextDouble();
+				isAlsoValid=false;
+			} catch (InputMismatchException e) {
+				isAlsoValid=true;
+			}
+			keyboard.nextLine();
+			
+		} while (isAlsoValid);
+		
+		do {
+			System.out.println("How many total hits did you get?");
+			try {
+				totalHits = keyboard.nextDouble();
+				validHitNumber=false;
+			} catch (InputMismatchException e) {
+				validHitNumber=true;
+			}
+			keyboard.nextLine();
+			
+		} while (validHitNumber);
+		
+		
+		
+		do {
+			for (int i = 0; i < totalHits; i++) {
 				double userInput = 0;
 				System.out.println("How many bases did you get on bat number " + (i + 1) + "?");
 				try {
@@ -52,8 +82,10 @@ public class BattingCalc {
 
 			
 		}
-		double battingPercentage=(hits/atBats);
-		System.out.println("Your batting percentage is: "+ battingPercentage);
+		battingPercentage=(totalHits/atBats);
+		System.out.println("Your batting percentage is: "+df.format(battingPercentage) );
+		sluggingPercentage=(hits/atBats);
+		System.out.println("Your slugging percentage is: "+ df.format(sluggingPercentage) );
 		
 		
 keyboard.close();
